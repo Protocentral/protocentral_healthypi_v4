e5d84b1bd502eb9abf262fc4b4e15966f228fe9c
Ashwin K Whitchurch <ashwin@protocentral.com>
2020-06-12 12:38:31 +0530

    Update upgrade.sh

--- docs/upgrade.sh
+++ docs/upgrade.sh
@@ -1,10 +1,12 @@
 healthypi_loc="https://github.com/Protocentral/protocentral_healthypi_v4/releases/latest/download/healthypi-rpi.zip"
 
+sudo rm -r --force *
+
 mkdir upgrade
 curl -LO "$healthypi_loc" 
 mv healthypi-rpi.zip upgrade/healthypi.zip
 
-sudo rm -r --force *
+
 
 rm -r upgrade/HealthyPi ||:
 rm -r upgrade/application.linux-armv6hf ||:
